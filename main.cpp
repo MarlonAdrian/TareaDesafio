2426f6cec66360bdb3d157253073b092cbb509de
Marlon Adrián Túquerres Romero <marlontuquerres.70@gmail.com>
2020-07-23 15:49:00 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a [5], b[5], i, k=0, h=0;
+
+for (i = 0; i<5; i++)
+{
+cout << "\nIngrese a["<< i<< "]=";
+cin >> a [i];
+}
+
+for (i=0; i<5; i++)
+{
+  if (a[i]%2 == 0)
+  {
+    b[k] = a[i]; k++;
+  }
+}
+
+cout << "\nLos numeros pares son:"<< endl;
+
+for (i=0;i<k;i++)
+{
+  cout << endl <<"b["<<i<<"]="<<b[i];
+}
+
+}
